415ac67f40e4855430755c08d1c250d0d99ae550
Alyona <liastra13@mail.ru>
2022-10-28 13:10:11 +0300

    fourth try of task 50

--- Task 50/Program.cs
+++ Task 50/Program.cs
@@ -29,52 +29,36 @@ void PrintArray(int[,] inArray)
     }
 }
 
-// void FindNumber(int[,] inArray, int n)
-// {
-//     for (int i = 0; i < inArray.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < inArray.GetLength(1); j++)
-//         {
-//             if(inArray[i, j] == n)
-//             {
-//                 Console.Write($"Искомое в массиве число {n} находится по адресу {i} строка, {j} столбец\t ");
-//                 break;
-//             }
-//             Console.WriteLine(); 
-//         }
-//     }
-//     Console.Write($"Искомого числа {n} в массиве нет");
-// }
-
-void FindNumber(int[,] inArray, int n)
+void FindNum(int[,] inArray, int n)
 {
     int i = 0;
     int j = 0;
     while (i < inArray.GetLength(0) && j < inArray.GetLength(1))
     {
-        if(inArray [i, j] == n)
+        if(inArray[i, j] != n)
         {
-            Console.WriteLine($"Искомое в массиве число {n} находится по адресу {i} строка, {j} столбец\t ");
             i++;
-            j++; 
+            j++;
         }
-        else
+        else if(inArray[i, j] == n)
         {
-            Console.Write($"Искомого числа {n} в массиве нет");
-            break;
+            Console.WriteLine($"Искомое число {n} в массиве находится в {i} строке и {j} столбце");
+        }
+        else 
+        {
+            Console.WriteLine($"Искомого числа {n} в массиве нет");
         }
     }
 }
 
-
 Console.Clear();
 Console.Write("Введите количество строк в массиве: ");
-int row = int.Parse(Console.ReadLine()!);
+int rows = int.Parse(Console.ReadLine()!);
 Console.Write("Введите количество столбцов в массиве: ");
 int columns = int.Parse(Console.ReadLine()!);
 Console.Write("Введите искомое в массиве число: ");
-int num = int.Parse(Console.ReadLine()!);
+int number = int.Parse(Console.ReadLine()!);
 
-int[,] array = GetArray(row, columns, 0, 10);
+int[,] array = GetArray(rows, columns, 0, 10);
 PrintArray(array);
-FindNumber(array, num);
+FindNum(array, number);
